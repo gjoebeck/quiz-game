c8a36ab6160299c84601f53c873722da82613369
gjoebeck <gjoebeck@gmail.com>
2022-11-06 21:03:30 -0800

    Initial fork

--- JS/game.js
+++ JS/game.js
@@ -1,23 +1,56 @@
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector('#progressText')
-const scoreText = document.querySelector('#score')
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
 let currentQuestion = {}
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let available = [];
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
 
 let questions = [
+    {
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 2,
+    },
+    {
+        question:
+            "The tallest building in the world is located in which city?",
+        choice1: "Dubai",
+        choice2: "New York",
+        choice3: "Shanghai",
+        choice4: "None of the above",
+        answer: 1,
+    },
+    {
+        question: "What percent of American adults believe that chocolate milk comes from brown cows?",
+        choice1: "20%",
+        choice2: "18%",
+        choice3: "7%",
+        choice4: "33%",
+        answer: 3,
+    },
+    {
+        question: "Approximately what percent of U.S. power outages are caused by squirrels?",
+        choice1: "10-20%",
+        choice2: "5-10%",
+        choice3: "15-20%",
+        choice4: "30%-40%",
+        answer: 1,
+    },
     {
         question: 'On what island did Ada Blackjack survive her harrowing ordeal?',
         choice1: 'Wake Island',
         choice2: 'Wrangel Island',
         choice3: 'Sentinel Island',
         choice4: 'Jarvis Island',
-        answer: 'Wrangel Island'
+        answer: 2,
     },
     {
         question: 'Where was the kingdom of Pontus located?',
@@ -25,7 +58,7 @@ let questions = [
         choice2: 'Southern Greece',
         choice3: 'Southwestern Syria',
         choice4: 'Western France',
-        answer: 'Northeastern Turkey'
+        answer: 1,
     },
     {
         question: 'Who was the first leader of the Ottoman Empire?',
@@ -33,7 +66,7 @@ let questions = [
         choice2: 'Murad I',
         choice3: 'Bayezid I',
         choice4: 'Osman I',
-        answer: 'Osman I'
+        answer: 4,
     },
     {
         question: 'When was Machu Picchu built?',
@@ -41,7 +74,7 @@ let questions = [
         choice2: 'The 13th Century',
         choice3: 'The 15th Century',
         choice4: 'The 10th Century',
-        answer: 'The 15th Century'
+        answer: 3,
     },
     {
         question: 'How long did the Han Dynasty rule over China?',
@@ -49,14 +82,72 @@ let questions = [
         choice2: '300 years',
         choice3: '900 years',
         choice4: '400 years',
-        answer: '400 years'
-    },
-    {
-        question: '',
-        choice1: '',
-        choice2: '',
-        choice3: '',
-        choice4: '',
-        answer: ''
-    },
+        answer: 4,
+    }
 ]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 9;
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
